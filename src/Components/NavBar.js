d783123688c692f9ed986d7e271585b594686873
Juan Musante <juanpedro.musante@gmail.com>
2022-07-14 22:52:24 -0300

    creacion de nuevo repo, check de posibles errores

---
+++ src/Components/NavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CartWidget from './CartWidget';
+
+function NavBar(){
+    return(
+        <header className="App-header shadow p-3 mb-5 bg-body rounded">
+            <h1 className="logo">TATT00 SH0P</h1>
+            <nav className="navegador">
+                <ul className="ul">
+                    <li className="li">
+                        <a href="filtro1.html">FILTRO 1</a>
+                    </li>
+                    <li className="li">
+                        <a href="filtro2.html">FILTRO 2</a>
+                    </li>
+                    <li className="li">
+                        <a href="filtro3.html">FILTRO 3</a>
+                    </li>
+                </ul>
+            </nav>
+            <CartWidget />
+        </header>
+    );
+}
+export default NavBar;
---
+++ src/Containers/ItemListContainer.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ItemCount from "../Components/ItemCount";
+import maquinas from "../Data/MockData";
+import ItemList from "../Components/ItemList";
+import Loading from "../Components/Loading";
+
+
+const ItemListContainer = ({ saludo }, props) => {
+  const [productList, setProductList] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  function loadingChange(){
+    setLoading(false);
+  }
+  setTimeout(loadingChange, 2000);
+
+  const productPromise = (task) => {
+    return new Promise((resolve, reject) => {
+        resolve(task);
+      });
+  }
+
+  if(loading){
+    return(
+      <Loading />
+    )
+  }else{
+  productPromise(maquinas)
+    .then(datos => {setProductList(datos)})
+
+  return (
+    <>
+      <div className="ItemListContainer">{saludo}</div>
+      <div className="products">
+        <ItemList items={productList} />
+      </div>
+      <ItemCount stock="5" initial="1" />
+    </>
+  );}
+};
+
+export default ItemListContainer;
